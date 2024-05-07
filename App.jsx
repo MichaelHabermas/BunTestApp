626b91e6f4c66b92d0d0b20aa909db6e2100ebad
habermoose <habermoose@gmail.com>
2024-05-07 11:58:06 -0400

    test4

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
-import { ReactElement, useEffect } from 'react';
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 
 SplashScreen.preventAutoHideAsync()
 
-export default function App(): ReactElement {
+export default function App() {
   useEffect(() => {
     async function prepare() {
       SplashScreen.hideAsync();
